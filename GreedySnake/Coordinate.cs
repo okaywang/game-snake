d6560bbaf1778212b36d4d3ff8914148a0ffd097
guojun <guojun@bsquare.com>
2013-11-22 07:03:49 +0800

    init project

---
+++ GreedySnake/Coordinate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfGreedySnake
+{
+   public  class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ GreedySnake/Food.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfGreedySnake
+{
+    class Food
+    {
+        public Coordinate Position { get; set; }
+    }
+}
