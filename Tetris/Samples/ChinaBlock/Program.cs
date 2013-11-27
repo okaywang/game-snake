74040d6481056f60ff243a07ed857b5d157e9795
wgj <402725796@qq.com>
2013-11-27 10:28:12 +0800

    add a url reference

--- Tetris/Samples/ChinaBlock/Program.cs
+++ Tetris/Samples/ChinaBlock/Program.cs
@@ -15,6 +15,7 @@ namespace ChinaBlock
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+//http://www.cnblogs.com/tuyile006/archive/2007/05/16/748256.html
         }
     }
 }
