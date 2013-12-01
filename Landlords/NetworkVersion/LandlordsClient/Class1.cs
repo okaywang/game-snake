37c3a89f51f7e0164b6a6908493c0ccc92d5381a
wgj <402725796@qq.com>
2013-12-01 12:35:16 +0800

    coding

---
+++ Landlords/NetworkVersion/LandlordsClient/Class1.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandlordsClient
+{
+    public class Class1
+    {
+    }
+}
