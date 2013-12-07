5093a9ba86c6964ab5119b305dc9ce4347895699
guojun <guojun@bsquare.com>
2013-12-06 18:30:34 -0700

    coding communication

---
+++ Utilities/Communication/Program.cs
@@ -0,0 +1,19 @@
+﻿using CommunicationTcpClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CommunicationTcpServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Trace.Listeners.Add(new System.Diagnostics.ConsoleTraceListener());
+            var cm = new Communicator();
+            cm.Connect();
+        }
+    }
+}
